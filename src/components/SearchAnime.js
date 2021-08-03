8365b20bf2a26b75679a9ddcdde4c10141bd5a35
aly3n <aly3n.dev@gmail.com>
2021-08-03 22:00:40 +0430

    search box improved, we are going to remove all of the lazy queris bcs they are useless

--- src/components/SearchAnime.js
+++ src/components/SearchAnime.js
@@ -1,11 +1,58 @@
+import { useApolloClient,gql } from '@apollo/client';
+import { useHistory } from "react-router-dom";
+import { detailQuery } from "../chooks/queries";
+import { useLazyQuery } from "@apollo/client";
+
 const SearchAnime = ({anime}) => {
+  const client = useApolloClient();
+  const history = useHistory();
+  const selectAnimeQuery = detailQuery;
+
+  // const [getAnime, { error, data }] = useLazyQuery(selectAnimeQuery);
+  // if (error) {
+  //   console.log(error.message);
+  //   return `Error! ${error}`;
+  // }
+  // if (data) {
+  //   //using set time out just BCS React is being bitch about to pushing to another page during getting the data
+  //   //the error was :  Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.
+  //   setTimeout(() => {
+  //     history.push(`/anime/${anime.id}`);
+  //   }, 1);
+  // }
+
+  const animeCardClickHandler = (e) => {
+    e.preventDefault();
+    
+    client.writeQuery({
+      query: gql`
+      query WriteIsLoading {
+        loadingbar {
+          isLoading
+        }
+      }`,
+      data: { // Contains the data to write
+        loadingbar: {
+          __typename: 'LoadingBar',
+          isLoading: 30
+        },
+      }
+    });
+        
+    // getAnime({ variables: { id: anime.id } });
+    setTimeout(() => {
+      
+      history.push(`/anime/${anime.id}`);
+    }, 500);
+
+  };
 
     return (
-        <div key={anime.id} className="flex justify-start items-center h-24 w-full cursor-pointer
+        <a href={`/anime/${anime.id}`} onClick={animeCardClickHandler} className=" flex justify-start items-center h-24 w-full cursor-pointer
         hover:text-indigo-900 hover:bg-indigo-200 transition-all">
           <img src={anime.coverImage.medium} className="object-cover h-full w-16" />
           <h4>{anime.title.english ? anime.title.english : anime.title.romaji}</h4>
-        </div>
+        </a>
     );
 }
 
--- src/pages/SearchList.js
+++ src/pages/SearchList.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import { menuSearchQuery } from "../chooks/queries";
 import { useLazyQuery } from "@apollo/client";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -8,8 +8,15 @@ import SearchAnime from "../components/SearchAnime";
 const SearchList = () => {
   const [searchValue, setSearchValue] = useState("");
   // const [searchData, setSearchData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(false);
   let searchData = [];
-
+ const expandingSearch = useCallback(
+    () => {
+      setIsExpanded(false)
+    },
+    [setIsExpanded],
+  )
   const searchChangeHandler = (e) => {
     console.log(e.target.value);
     setSearchValue(e.target.value);
@@ -20,7 +27,22 @@ const SearchList = () => {
   };
 
   useEffect(() => {
+    const windowClickOutOfSearch = (e) => {
+      if(e.target.classList.contains('search-click')){
+        setIsExpanded(true);
+      } else {
+        setIsExpanded(false);
+      }
+    }
+    window.addEventListener('click', windowClickOutOfSearch);
+
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsExpanded(true);
     const searchTimer = setTimeout(() => {
+      setIsLoading(false);
       getSearchAnime({ variables: { search: searchValue } });
     }, 700);
     return () => {
@@ -37,24 +59,24 @@ const SearchList = () => {
 
   if (data) {
     searchData = data.Page.media;
-    console.log(searchData);
   }
 
   return (
-    <div className=" relative z-50 h-10 w-48 focus-within:w-64
-     transition-all duration-1000   flex justify-center items-center border-0 rounded-3xl bg-indigo-800 ">
+    <div className="search-click relative z-50 h-10 w-48 focus-within:w-64
+     transition-all duration-1000 flex justify-center items-center border-0 rounded-3xl bg-indigo-800 ">
 
-      <form className=" w-full h-full flex justify-between items-center z-50">
-      <span className="text-gray-500 cursor-pointer hover:text-indigo-200 bg-indigo-800 rounded-l-3xl
-          text-2xl h-full w-10 pl-3 pr-2 pt-px flex justify-center items-center"><FontAwesomeIcon  icon={faSearch} /></span>
-        <input className="bg-indigo-800 h-full w-full focus:outline-none focus:bg-indigo-200 px-1 rounded-r-3xl"
+      <form className="search-click w-full h-full flex justify-between items-center z-50">
+      <span className="search-click text-gray-500 cursor-pointer hover:text-indigo-200 bg-indigo-800 rounded-l-3xl
+          text-2xl h-full w-10 pl-3 pr-2 pt-px flex justify-center items-center"><FontAwesomeIcon className="search-click"  icon={faSearch} /></span>
+        <input className="search-click bg-indigo-800 h-full w-full focus:outline-none focus:bg-indigo-200 px-1 rounded-r-3xl"
          type="text" value={searchValue} placeholder={"Search..."} onChange={searchChangeHandler} />
       </form>
 
 
-      {(searchData.length >= 1) && <div className="absolute w-full left-0 top-5 bg-indigo-900 z-40">
-        <div className="flex flex-col items-start justify-start mt-7 text-white ">
-      {searchData.map(anime=> {
+      {isExpanded && <div className="search-click absolute w-full left-0 top-5 bg-indigo-900 z-40" style={{minHeight:"120px"}}>
+        <div className="search-click flex flex-col items-start justify-start mt-7 text-white ">
+       {isLoading && <div class="search-click lds-dual-ring mx-auto"></div>}
+      {!isLoading && searchData.map(anime=> {
         return (<SearchAnime key={anime.id} anime={anime} />)
       })}
         </div>
